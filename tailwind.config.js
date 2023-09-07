5bdb2f4b0c102a0bc51ee4511a428b5810f01d9e
kyyyon <mreyes@student.hau.edu.ph>
2023-09-08 00:38:45 +0800

    added events page and updated landmark3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        cards: "repeat(auto-fit, minmax(250px, 1fr))",
+      },
       fontSize: {
         "10xl": "12rem",
       },
